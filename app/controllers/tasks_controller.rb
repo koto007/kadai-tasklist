17a22eb3bfb5f6f251dc1482f514ab79d18d50af
joséphine <cotofrancejapon@gmail.com>
2019-03-14 11:23:57 +0000

    third commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -36,7 +36,7 @@ end
 def update 
     @task = Task.find(params[:id])
     
-    if @task.update(task.params)
+    if @task.update(task_params)
         flash[:success] = 'タスクは正常に更新されました'
         redirect_to @task
         
