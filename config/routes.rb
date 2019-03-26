90e0c8163d8e6a47b0e897dbe36ce7acd048c4ba
joséphine <cotofrancejapon@gmail.com>
2019-03-26 11:24:22 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
   
-  root to: 'tasks#index'
+  get 'toppages/index'
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
   
   #get 'tasks/:id', to: 'tasks#show'
   #post 'tasks', to: 'tasks#create'
@@ -13,6 +30,11 @@ Rails.application.routes.draw do
   #get 'tasks/new', to: 'tasks#new'
   
   #get 'tasks/:id/edit', to: 'tasks#edit'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
+  resources :users, only: [:index, :show, :new, :create]
   resources :tasks
 end
